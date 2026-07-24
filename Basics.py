5a9758932691cb29db6e0fe0abbcee0106b69171
Varsha sri Siva <varshsiva11@gmail.com>
2026-07-24 21:33:22 +0530

    Add Python basics tutorial in Basics.py

---
+++ Basics.py
@@ -0,0 +1,59 @@
+# ==========================================
+# Day 1 - Python Basics
+# Author : Varsha sri Siva
+# Topic  : Variables, Data Types, Input, Output, Operators
+# ==========================================
+
+# 1. Print Statement
+print("Hello, World!")
+print("Welcome to Python Programming!")
+
+print("\n-----------------------------")
+
+# 2. Variables
+name = "Varsha"
+age = 20
+college = "ABC Engineering College"
+
+print("Name :", name)
+print("Age :", age)
+print("College :", college)
+
+print("\n-----------------------------")
+
+# 3. Data Types
+student_name = "Varsha"      # String
+student_age = 20             # Integer
+cgpa = 8.5                   # Float
+is_student = True            # Boolean
+
+print("Data Types")
+print(type(student_name))
+print(type(student_age))
+print(type(cgpa))
+print(type(is_student))
+
+print("\n-----------------------------")
+
+# 4. Taking Input from User
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: ")) #Type casting
+
+print("Hello", user_name)
+print("You are", user_age, "years old.")
+
+print("\n-----------------------------")
+
+# 5. Arithmetic Operators
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition       :", num1 + num2)
+print("Subtraction    :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division       :", num1 / num2)
+print("Modulus        :", num1 % num2)
+
+print("\n-----------------------------")
+
+# Day 1 Completed
